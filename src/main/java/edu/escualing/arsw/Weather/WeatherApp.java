3120d3922620db8189eb126de800d99ec6574cc1
Guillermo Castro <guillermo.castro@mail.escuelaing.edu.co>
2021-04-07 07:13:09 -0500

    pom y app a correr

---
+++ src/main/java/edu/escualing/arsw/Weather/WeatherApp.java
@@ -0,0 +1,13 @@
+package edu.escualing.arsw.Weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class WeatherApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApp.class, args);
+    }
+
+    }
+}
